48977533c10a7b119f30eb93d7936f7b9db941ef
PurvaK <purva.k@simformsolutions.com>
2023-03-09 16:51:10 +0530

    migration updates

--- db/migrate/20230309110627_create_users.rb
+++ db/migrate/20230309110627_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-
+      t.string :first_name
+      t.string :last_name
+      t.string :contact
+      t.string :address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_09_110627) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

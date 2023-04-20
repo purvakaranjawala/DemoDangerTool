650dc568f8b1a3f4f40cd63d63336677b4fbfbe5
PurvaK <purva.k@simformsolutions.com>
2023-04-20 16:46:31 +0530

    Test rubocop inline messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def new
+    
+  end
+
+  private
+
+  def user_params
+    params.permit(:name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  scope: active, -> (name) { where(name: name)}
+  # scope: active -> (name) { where(name: name) }
+
+  after_create :test_created
+  before_save :test_updated
 end
